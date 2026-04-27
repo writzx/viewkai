228e78b47f859f6dbacd2467602982cef078bf6f
Writwick <writzx@gmail.com>
2026-04-27 00:07:30 +0000

    style: rustfmt lib.rs (Plan 03.25)

--- crates/viewkai/src/lib.rs
+++ crates/viewkai/src/lib.rs
@@ -1369,7 +1369,7 @@ impl Viewer {
                         None
                     };
                     if let Some(target_rect) = target_rect {
-            ui.scroll_to_rect(target_rect, Some(egui::Align::TOP));
+                        ui.scroll_to_rect(target_rect, Some(egui::Align::TOP));
                     }
                 }
 
